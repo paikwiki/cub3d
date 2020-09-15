7ce59397f2d5f256513e8b8e50df43997d8aefeb
paikwiki <paikwiki@gmail.com>
2020-09-15 18:00:51 +0900

    Create check_line()

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: paikwiki <paikwiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 15:12:02 by paikwiki          #+#    #+#             */
-/*   Updated: 2020/09/14 18:43:29 by paikwiki         ###   ########.fr       */
+/*   Updated: 2020/09/15 17:57:57 by paikwiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,18 @@ void	exit_puterr(const char *msg)
 	exit(EXIT_FAILURE);
 }
 
+void 	check_line(char *line)
+{
+	printf("%s\n", line);
+}
+
 int		main(int argc, char *argv[])
 {
 	char	*line;
 	int		fd;
 	int		rd_byte;
 	int 	is_done;
-	t_list  *temp_line;
+	t_list  *lst_line;
 
 	if (argc < 2)
 	{
@@ -52,13 +57,14 @@ int		main(int argc, char *argv[])
 		{
 			if ((rd_byte = get_next_line(fd, &line)) <= 0)
 				is_done = TRUE;
-			if (!temp_line)
-				temp_line = ft_lstnew(line);
+			check_line(line);
+			if (!lst_line)
+				lst_line = ft_lstnew(line);
 			else
-				ft_lstadd_back(&temp_line, ft_lstnew(line));
-			printf("%d\n", ft_lstsize(temp_line));
+				ft_lstadd_back(&lst_line, ft_lstnew(line));
+			free(line);
 		}
-		free(line);
+		printf("%d\n", ft_lstsize(lst_line));
 	}
 	else
 		exit_puterr("Fail to open a map file.");
