96225b05f3ad2a8378604e40fc1a79f022706929
paikwiki <paikwiki@gmail.com>
2020-09-18 19:31:17 +0900

    Don't exec put_map()

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: paikwiki <paikwiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 15:12:02 by paikwiki          #+#    #+#             */
-/*   Updated: 2020/09/18 16:39:11 by paikwiki         ###   ########.fr       */
+/*   Updated: 2020/09/18 18:12:41 by paikwiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int		main(int argc, char *argv[])
 	free(lines);
 	check_map_horizon(map, &note);
 	check_map_vertical(map, &note);
-	put_map(map, &note);
-	ft_putstr("Good\n");
+//	put_map(map, &note);
+//	ft_putstr("Good\n");
 	return (0);
 }
