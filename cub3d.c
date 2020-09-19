12939a6c310a0b87ee2fc19614292ac421515f68
paikwiki <paikwiki@gmail.com>
2020-09-19 20:34:14 +0900

    Extract the process of map parsing

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: paikwiki <paikwiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 15:12:02 by paikwiki          #+#    #+#             */
-/*   Updated: 2020/09/19 20:26:52 by paikwiki         ###   ########.fr       */
+/*   Updated: 2020/09/19 20:34:02 by paikwiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ int		key_press(int keycode)
 	if (keycode == KEY_ESC)
 		exit(0);
 	return (0);
+}
+
+void	process_map(char **map, t_note *note, t_list **lines)
+{
+	map[note->map_height] = 0;
+	init_map(map, note);
+	set_map(map, lines);
+	check_map_horizon(map, note);
+	check_map_vertical(map, note);
 }
 
 int		main(int argc, char *argv[])
@@ -36,13 +45,8 @@ int		main(int argc, char *argv[])
 	if (note.is_map == FALSE)
 		exit_puterr("Fail to get information of map_size.");
 	map = (char **)malloc(sizeof(char *) * note.map_height + 1);
-	map[note.map_height] = 0;
-	init_map(map, &note);
-	set_map(map, &lines);
+	process_map(map, &note, &lines);
 	free(lines);
-	check_map_horizon(map, &note);
-	check_map_vertical(map, &note);
-
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, note.info_r[0], note.info_r[1], "cub3D");
 	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &note);
